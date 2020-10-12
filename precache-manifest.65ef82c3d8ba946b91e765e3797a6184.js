be8c5c46f8b83efe95595ea40e5e90f4b1f6705d
BorisovecViktor <viktorborisovets@gmail.com>
2020-10-12 11:01:40 +0300

    Updates

---
+++ precache-manifest.65ef82c3d8ba946b91e765e3797a6184.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5def18cec1cdd84bea734ecdc3d3078",
+    "url": "./index.html"
+  },
+  {
+    "revision": "49fddab4a02850aba754",
+    "url": "./static/css/main.f0c88e38.chunk.css"
+  },
+  {
+    "revision": "de24a6fca52fd2aecef5",
+    "url": "./static/js/2.f7aa3336.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.f7aa3336.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49fddab4a02850aba754",
+    "url": "./static/js/main.bf07758b.chunk.js"
+  },
+  {
+    "revision": "8314e3554cb354590f57",
+    "url": "./static/js/runtime-main.05d1baf0.js"
+  },
+  {
+    "revision": "482e37260cd688576287f1976e958e0b",
+    "url": "./static/media/Mont-Bold.482e3726.otf"
+  },
+  {
+    "revision": "f0f845588eef439ac6ecc9b6ab42191c",
+    "url": "./static/media/Mont-Regular.f0f84558.otf"
+  },
+  {
+    "revision": "5ca5c1f58aa3bcefb2bf88439be8fe51",
+    "url": "./static/media/arrow-left.5ca5c1f5.svg"
+  },
+  {
+    "revision": "d5f941ebd8ca6134fbbbae1e936f6a34",
+    "url": "./static/media/sad-cry.d5f941eb.svg"
+  }
+]);
